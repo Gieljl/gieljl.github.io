0cb7f93a3c8dbafe782404b8857fd092db6100a6
gieljl <giel.janssenlok@alliander.com>
2022-02-26 23:27:56 +0100

    redux template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import yasatlogo from './yasa7.png';
+import logo from './yasa7.png';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={yasatlogo} className="App-logo" alt="logo" />
-        <p>
-          Coming soon
-        </p>
-        
-        <a
-          className="App-link"
-          href="http://roderikvandelogt.nl/yasat/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yassat v1
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
       </header>
     </div>
   );
